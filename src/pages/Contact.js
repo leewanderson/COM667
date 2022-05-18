919b7069f925433c9bf16abf3b073ae3a6d13b3b
Lee Anderson <leewanderson@yahoo.co.uk>
2022-05-18 18:56:14 +0100

    Added Contact Form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,34 +1,34 @@
-import React from "react";
+import React, { useRef } from 'react';
 import v1Background from "../assets/v1background.jpg";
+import emailjs from "emailjs-com";
 import "../styles/Contact.css";
 
-function Contact() {
-  return (
-    <div className="contact">
-      <div
-        className="leftSide"
-        style={{ backgroundImage: `url(${v1Background})` }}
-      ></div>
-      <div className="rightSide">
-        <h1> Contact Us</h1>
+export const Contact = () => {
+  const form = useRef();
 
-        <form id="contact-form" method="POST">
-          <label htmlFor="name">Full Name</label>
-          <input name="name" placeholder="Enter full name..." type="text" />
-          <label htmlFor="email">Email</label>
-          <input name="email" placeholder="Enter email..." type="email" />
-          <label htmlFor="message">Message</label>
-          <textarea
-            rows="6"
-            placeholder="Enter message..."
-            name="message"
-            required
-          ></textarea>
-          <button type="submit"> Send Message</button>
-        </form>
-      </div>
-    </div>
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_uqsob1u', 'template_vvbvrub', e.target, 'oL9m_NAR2M2jCkvC6')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
-}
+};
 
 export default Contact;
