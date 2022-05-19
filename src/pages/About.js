7b443406d9051a11b9c6d168e6be30b33d97a97e
Lee Anderson <leewanderson@yahoo.co.uk>
2022-05-19 23:51:30 +0100

    added content to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,13 +9,24 @@ function About() {
         style={{ backgroundImage: `url(${MonitoringImage})` }}
       ></div>
       <div className="aboutBottom">
-        <h1> ABOUT US</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-          fugiat iusto fuga praesentium optio, eaque rerum!
-        </p>
+        <h1>About Onboarding Site</h1>
+        <p>This project was created with the intention of allowing new starts to get up to speed with all projects.</p>
+        <p>Each project will contain a "Points of Contact" list. For any queries regarding this website, use the contact form to lodge a question or piece of feedback.</p>
+        <p>For any more urgent queries, please refer to the contacts listed below</p>
+        <h2>Points of Contact</h2>
+        <table>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Note</th>
+        </tr>
+        <tr>
+            <td>Lee Anderson</td>
+            <td>lee.anderson@version1.com</td>
+            <td>For any queries regarding site content and feature requests</td>
+        </tr>
+        </table>
+        <p><br></br></p>
       </div>
     </div>
   );
